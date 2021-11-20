2a250bc4a79f229835952fa489d0fb5f37367a8b
Eduardo-Mercado <mercadohedward@gmail.com>
2021-11-20 00:01:51 -0600

    Sumatoria de letras
    proyecto de sumar palabras basadas en el valor que posee cada letra perteneciente al alfabeto ingles.

---
+++ SumaCaracteres/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumaCaracteres
+{
+	class Program
+	{
+		private static Dictionary<char, int> Alfabeto = new Dictionary<char, int>();
+
+		static void Main(string[] args)
+		{
+			InicializarRandomDelAlfabeto();
+			
+			string palabra = "";
+			while (true)
+			{
+				Console.WriteLine("Digite una palabra :");
+				palabra = Console.ReadLine();
+				Console.WriteLine("Valor de la palabra = " + palabra + " es  = " + palabra.ToCharArray().Sum(c => Alfabeto[char.ToLower(c)]) + "");
+			}
+		}
+
+		private static void InicializarRandomDelAlfabeto()
+		{
+			char[] letras = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+			List<int> valores = Enumerable.Range(1, 26).ToList();
+			Random random = new Random();
+			char[] alfabetoDesordenado =  letras.OrderBy(o => random.Next()).ToArray();
+
+			Alfabeto = alfabetoDesordenado.Select(y => new { letra = y, valor = valores[Array.IndexOf(alfabetoDesordenado, y)] }).ToDictionary(k => k.letra, k => k.valor);
+		}
+
+	}
+}
